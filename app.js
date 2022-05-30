e3fe54250583ed1c477da72b01553e81d1314831
MSKose <saitmustafa1998@gmail.com>
2022-05-30 21:40:41 +0200

    added the basic html and some js variables

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log('**** app.js *******');
+
+const dilInput = document.querySelector('.diller');
+const silBtn = document.getElementById('sil');
+const ekleBtn = document.getElementById('ekle');
+
+const dilSection = document.querySelector('#dil-section');
+
+const ul = document.createElement('ul');
+dilSection.appendChild(ul);
